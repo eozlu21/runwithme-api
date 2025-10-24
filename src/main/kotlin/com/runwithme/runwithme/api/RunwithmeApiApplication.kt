321e5ab9bfb96dbee8c56b0cce7e13ce8cc405b6
Ege Erdem Özlü <eozlu21@ku.edu.tr>
2025-10-25 00:01:31 +0300

    create general scaffold

---
+++ src/main/kotlin/com/runwithme/runwithme/api/RunwithmeApiApplication.kt
@@ -0,0 +1,11 @@
+package com.runwithme.runwithme.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RunwithmeApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<RunwithmeApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/runwithme/runwithme/api/RunwithmeApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.runwithme.runwithme.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RunwithmeApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
